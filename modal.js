ba8b7cca157d22003c2a1a2237bafab6f8dd42b5
Ori <ori.nahu@gmail.com>
2022-02-02 16:33:14 +0200

    Modal improvements

--- modal.js
+++ modal.js
@@ -1,6 +1,7 @@
 class Modal extends HTMLElement {
   constructor() {
     super();
+    this.isOpen = false;
     this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
           <style>
@@ -16,9 +17,20 @@ class Modal extends HTMLElement {
                 pointer-events: none;
             }
 
+            :host([opened]) #backdrop,
+            :host([opened]) #modal {
+              opacity: 1;
+              pointer-events: all;
+            }
+
+            :host([opened]) #modal {
+              top: 15vh;
+            }
+            
+          
             #modal {
                 position: fixed;
-                top: 15vh;
+                top: 10vh;
                 left: 25%;
                 width: 50%;
                 z-index: 100;
@@ -30,6 +42,7 @@ class Modal extends HTMLElement {
                 justify-content: space-between;
                 opacity: 0;
                 pointer-events: none;
+                transition: all 0.3s ease-out;
             }
 
             #actions {
@@ -45,11 +58,13 @@ class Modal extends HTMLElement {
             }
 
             header {
-                padding: 1rem;
+               padding: 1rem;
+               border-bottom: 1px solid #ccc;
             }
 
-            header h1 {
-                font-size: 2.5rem;
+            ::slotted(h1) {
+                font-size: 2rem;
+                margin: 0;
             }
 
             #main {
@@ -62,33 +77,68 @@ class Modal extends HTMLElement {
           <div id="backdrop"></div>
           <div id="modal">
             <header>
-              <h1>Please Confirm</h1>
+              <slot name="title">Header</slot>
             </header>
             <section id="main">
               <slot></slot>
-            <section>
+            </section>
             <section id="actions">
-              <button>Cancel</button>
-              <button>Action</button>
+              <button id="cancel-btn">Cancel</button>
+              <button id="confirm-btn">Action</button>
             <section>
           </div>
         `;
+        
+    // const slots = this.shadowRoot.querySelectorAll("slot");
+    // slots[1].addEventListener("slotchange", (event) => {
+    //   console.dir(slots[1].assignedNodes());
+    // });
+
+    const backdrop = this.shadowRoot.querySelector("#backdrop");
+    const cancelButton = this.shadowRoot.querySelector("#cancel-btn");
+    const confirmButton = this.shadowRoot.querySelector("#confirm-btn");
+
+    backdrop.addEventListener('click' , this._cancel.bind(this))
+    cancelButton.addEventListener("click", this._cancel.bind(this));
+    confirmButton.addEventListener("click", this._confirm.bind(this));
+
   }
 
-//   attributeChangedCallback(name, oldValue, newValue) {
-//     if (name === 'opened') {
-//         if(this.hasAttribute('opened')) {
-//             this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
-//             this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
-//             this.shadowRoot.querySelector('#modal').style.opacity = 1;
-//             this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
-//         }
-//     }
-//   }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.hasAttribute("opened")) {
+      this.isOpen = true;
+    } else {
+      this.isOpen = false;
+    }
+  }
 
-//   static get observedAttributes() {
-//     return ["opened"];
-//   }
-// }
+  static get observedAttributes() {
+    return ["text"];
+  }
+
+  open() {
+    this.setAttribute("opened", "");
+    this.isOpen = true;
+  }
+
+  hide() {
+    if (this.hasAttribute("opened")) {
+      this.removeAttribute("opened");
+    }
+    this.isOpen = false;
+  }
+
+  _cancel(event) {
+    this.hide();
+    const cancelEvent = new Event('cancel' , { bubbles: true , composed: true});
+    event.target.dispatchEvent(cancelEvent);
+  }
+
+  _confirm() {
+    this.hide();
+    const confirmEvent = new Event('confirm');
+    this.dispatchEvent(confirmEvent);
+  }
+}
 
 customElements.define("uc-modal", Modal);
